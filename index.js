8108fca7cb1cf628a6adb65ed469a8eddf444d70
Alberto Sánchez <albertoensan@gmail.com>
2020-01-11 12:47:20 +0100

    Cambios en local funcionando

--- index.js
+++ index.js
@@ -2,22 +2,20 @@
 const mergeJSON = require('merge-json');
 
 module.exports = class xrequire {
-    constructor(conf, config) {
-        var defaultConfig = {
-            "PRO" : true,   // If false logs won't do anything
-            "DEV" : ".."
-        };
-        this._c = mergeJSON.merge(defaultConfig, config);
-        if (!this._c.hasOwnProperty(conf))
-            this._conf = PRO;
-        else
-            this._conf = conf;
-
+    constructor(config = {}) {
+        if (!global.hasOwnProperty("extendedrequirer")) {
+            var defaultConfig = {
+                "currentConfig" : "PRO",
+                "DEV": ".."
+            };
+            var _conf = "PRO";
+            global.extendedrequirer  = mergeJSON.merge(defaultConfig, config)
+        }
     }
 
-    require(module){
-        if (this._conf == "PRO")
+    require(module) {
+        if (global.extendedrequirer.currentConfig == "PRO")
             return require(module);
-        return require( this._c[this._conf] + "/" + module + "/index.js");
+        return require(global.extendedrequirer[global.extendedrequirer.currentConfig] + "/" + module + "/index.js");
     }
 }
